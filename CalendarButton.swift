dc30e00eb87c2f0c50e202936f2c8d839c3a17e3
R34P3R44 <t.adrian88@gmail.com>
2021-09-05 22:28:22 +0100

    Updated Calendar button

---
+++ CalendarButton.swift
@@ -0,0 +1,31 @@
+//
+//  CalendarButton.swift
+//  Uptodate
+//
+//  Created by Adrian.T on 03/06/2021.
+//
+
+import UIKit
+
+class CalendarButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitle("Calendar", for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        titleLabel?.textColor = .white
+        layer.cornerRadius = 7.0
+        backgroundColor = UIColor.systemIndigo
+    }
+}
---
+++ ScanButton.swift
@@ -0,0 +1,31 @@
+//
+//  ScanButton.swift
+//  Uptodate
+//
+//  Created by Adrian.T on 02/06/2021.
+//
+
+import UIKit
+
+class ScanButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitle("Scan", for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        titleLabel?.textColor = .white
+        layer.cornerRadius = 7.0
+        backgroundColor = UIColor.systemIndigo
+    }
+}
