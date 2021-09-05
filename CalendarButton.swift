4d0a6dece380114d6bac1efb5044fda6c835109c
R34P3R44 <t.adrian88@gmail.com>
2021-09-05 12:06:34 +0100

    Initial Commit

---
+++ CalendarButton.swift
@@ -0,0 +1,31 @@
+//
+//  CalendarButton.swift
+//  Uptodate
+//
+//  Created by Adrian.T on 03/06/2021.
+//
+
+/*import UIKit
+
+class CalendarButton: UIbutton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translateAutoresizingMaskIntoConstrains = false
+        setTitle("Calendar", for: .normal)
+        titleLabel?.font = UIfont.boldSystemFont(ofIsze: 18.0)
+        titleLabel?.textColor = .white
+        layer.cornerRadius = 7.0
+        backgorundColor = UIColor.systemIndigo
+    }
+}*/
